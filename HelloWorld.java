0991ab5820b326b65b5da288f9883820b7a8ae4a
mchudasama <mchudasama@logilite.com>
2020-07-15 12:23:43 +0530

    What is your Name

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("***************************");
 	System.out.println("Hello, World");
+	System.out.println("Hello, User");
+	System.out.println("What is your Name");
+	System.out.println("What is your Age");
 	System.out.println("***************************");
     }
 }
