0a8dfb25b760a411898a77c70c89b1e0befd2ba8
mchudasama <mchudasama@logilite.com>
2020-07-17 09:58:36 +0530

    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ class HelloWorld {
     public static void main(String[] args) {
 	System.out.println("***************************");
 	System.out.println("Hello, World");
-	System.out.println("***************************");
+	System.out.println("Hello, User");
+	System.out.println("What is your Name");
+	System.out.println("What is your Age");
     }
 }
